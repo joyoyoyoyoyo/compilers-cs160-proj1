86f16d6e95e147a5d8a9dc77ca369656bbe32d16
Angel Ortega <an22ortega@gmail.com>
2017-04-07 15:01:25 -0700

    executable-hotfix

--- max.hpp
+++ max.hpp
@@ -1,12 +1,19 @@
 #ifndef _MAX_HPP
 #define _MAX_HPP
 
+#include <limits>
+#include <cstdint>
+
 #include "tree.hpp"
 
 // You will need to implement a complete visitor that
 // finds the maximum element in the tree. You can use
 // the print and sum visitors for reference.
-
-// WRITEME
+class MaxVisitor : public Visitor {
+public:
+    int max; // start with base minimum, then set to max value found
+    MaxVisitor() : max(INT32_MIN) {}
+    void visitNode(Node* node);
+};
 
 #endif
